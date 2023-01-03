b98f56c461d07ba32c6686d73327d2a2f032e71e
ysyx_22040343-Chen Tingrun <chentingrun1011@163.com>
2023-01-03 11:21:50 +0800

    difftest finish and pass all test TODO: register order ???

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -3,7 +3,11 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for (int i = 0; i < 32; i++) {
+      if (cpu.gpr[i] != ref_r->gpr[i]) return false;
+  }
+  if (cpu.pc != ref_r->pc) return false;
+  return true;
 }
 
 void isa_difftest_attach() {
