a00c073d51a4d73d852b835ce851f91764e8d03f
ysyx_22040343-Chen Tingrun <chentingrun1011@163.com>
2023-02-07 13:28:48 +0800

    sound card finish, random noise bug still unfix

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -7,20 +7,35 @@
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
+#define AUDIO_DATA_ADDR      (AUDIO_ADDR + 0x18)
 
 void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
+  outl(AUDIO_INIT_ADDR, 1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  Area audio_buf = ctl->buf;
+  uint32_t audio_buf_len = audio_buf.end - audio_buf.start;
+  volatile int sbuf_free = inl(AUDIO_SBUF_SIZE_ADDR) - inl(AUDIO_COUNT_ADDR);
+  while (sbuf_free < audio_buf_len) {
+    sbuf_free = inl(AUDIO_SBUF_SIZE_ADDR) - inl(AUDIO_COUNT_ADDR);
+  }
+  for (int i = 0; i < audio_buf_len; i++) {
+    outb(AUDIO_DATA_ADDR, *(uint8_t *)(audio_buf.start + i));
+  }
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -9,24 +9,89 @@ enum {
   reg_sbuf_size,
   reg_init,
   reg_count,
+  reg_data,
   nr_reg
 };
 
+static SDL_AudioSpec s = {};
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+static int audio_head;
+static int audio_tail;
+
+static void sbuf_enqueue(uint8_t data) {
+    if (!((audio_tail + 1) % (CONFIG_SB_SIZE - 1) == audio_head)) {
+        audio_base[reg_count]++;
+        sbuf[audio_tail] = data;
+        audio_tail = (audio_tail + 1) % (CONFIG_SB_SIZE - 1);
+    } else {
+        printf("queue full !!!! \n");
+    }
+}
+
+static uint8_t sbuf_dequeue() {
+    audio_base[reg_count]--;
+    uint8_t data = sbuf[audio_head];
+    audio_head = (audio_head + 1) % (CONFIG_SB_SIZE - 1);
+    return data;
+}
+
+void sdl_audio_callback(void *userdata, uint8_t* stream, int len) {
+    int nlen = audio_base[reg_count];
+    if (len > nlen) {
+        for (int i = 0; i < nlen; i++) {
+            stream[i] = sbuf_dequeue();
+        }
+        for (int i = nlen; i < len; i++) {
+            stream[i] = 0;
+        }
+    } else {
+        for (int i = 0; i < len; i++) {
+            stream[i] = sbuf_dequeue();
+        }
+    }
+}
+
+void init_sdl_audio(int freq, uint8_t channels, uint16_t samples) {
+    s.freq = freq;
+    s.channels = channels;
+    s.samples = samples;
+    s.callback = sdl_audio_callback;
+    audio_tail = 0;
+    audio_head = audio_tail;
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
+}
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+    if (offset == reg_init * 4 && is_write && len == 4) {
+        int freq = audio_base[reg_freq];
+        uint8_t channels = audio_base[reg_channels];
+        uint16_t samples = audio_base[reg_samples];
+        init_sdl_audio(freq, channels, samples);
+        audio_base[reg_init] = (uint32_t)0;
+        audio_base[reg_count] = 0;
+    }
+    if (offset == reg_sbuf_size * 4 && !is_write && len == 4) {
+        audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+    }
+    if (offset == reg_count * 4 && !is_write && len == 4) {
+    }
+    if (offset == reg_data * 4 && is_write && len == 1) {
+        sbuf_enqueue(audio_base[reg_data]);
+    }
 }
 
 void init_audio() {
-  uint32_t space_size = sizeof(uint32_t) * nr_reg;
-  audio_base = (uint32_t *)new_space(space_size);
+    uint32_t space_size = sizeof(uint32_t) * nr_reg;
+    audio_base = (uint32_t *)new_space(space_size);
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
+    add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
 #else
-  add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
+    add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
 #endif
 
-  sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+    sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
+    add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
 }
