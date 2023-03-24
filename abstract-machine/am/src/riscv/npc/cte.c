d9cbd3c9d3dd18e504cf13af3f598050399d57af
ysyx_22040343-Chen Tingrun <chentingrun1011@163.com>
2023-03-24 16:41:24 +0800

    npc am

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -1,4 +1,5 @@
 #include <am.h>
+#include <riscv/riscv.h>
 #include <klib.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
@@ -7,7 +8,30 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      default: ev.event = EVENT_ERROR; break;
+      case (0xb):
+          if (c->gpr[17] == -1) {
+              ev.event = EVENT_YIELD;
+          } else if (c->gpr[17] == 1 ||
+                     c->gpr[17] == 0 ||
+                     c->gpr[17] == 4 ||
+                     c->gpr[17] == 9 ||
+                     c->gpr[17] == 2 ||
+                     c->gpr[17] == 3 ||
+                     c->gpr[17] == 4 ||
+                     c->gpr[17] == 7 ||
+                     c->gpr[17] == 8 ||
+                     c->gpr[17] == 13 ||
+                     c->gpr[17] == 19) {
+              ev.event = EVENT_SYSCALL;
+          } else {
+              printf("Please handle me, a7=%d\n", c->gpr[17]);
+          }
+          c->mepc = c->mepc + 4;
+          break;
+      default:
+          printf("%s\t0x%d\n", "mcause", c->mcause);
+          // printf("%s\t0x%d\n", "mepc", c->mepc);
+          ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
@@ -22,10 +46,8 @@ extern void __am_asm_trap(void);
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
-
   // register event handler
   user_handler = handler;
-
   return true;
 }
 
